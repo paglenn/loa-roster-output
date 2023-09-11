96e555fcef0a5fb286455e81a4e378016d5db7ac
Paul Glenn <paglenn@users.noreply.github.com>
2023-09-11 18:07:39 -0400

    commonJS vs ES6 bug fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 // server.js
 // turn on server
-import app from "./index";
+const app = require("./index");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT ?? 3000;
