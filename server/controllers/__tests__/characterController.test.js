a9b06149f392f99f9488a4a9516447bd6efa5a42
Paul Glenn <paglenn@users.noreply.github.com>
2023-10-02 15:36:40 -0400

    created integrated install scripts to facilitate testing and server-client separation

--- server/controllers/__tests__/characterController.test.js
+++ server/controllers/__tests__/characterController.test.js
@@ -2,7 +2,7 @@ const { connection } = require("../../database").mongoose;
 const characterController = require("../characterController");
 const Character = require("../../database/models/characterModel");
 
-const originalIlvl = 1415;
+const originalIlvl = 1430;
 
 const testChar = {
   name: "Technosaint",
