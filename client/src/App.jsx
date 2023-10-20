e3de2f763287645a98206721e15d0fa0a8674e31
Paul Glenn <paglenn@users.noreply.github.com>
2023-10-20 18:20:06 -0400

    reverted to hash router for refreshing since app wasn't refreshing otherwise. will need to rethink how to configure with browser router in a more general way

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import MainPage from "./displays/MainPage";
 import { Login, Signup } from "./features/auth";
 import Header from "./components/Header";
+import { checkAdmin } from "./utils/api";
 const App = () => {
+  const currentUser = localStorage.getItem("user") ?? "";
   const [user, setUser] = useState("");
+  useEffect(() => {
+    if (currentUser !== "test") setUser(currentUser);
+    else if (checkAdmin()) setUser(currentUser);
+  }, []);
   return (
     <div className=" bg-slate-800 flex flex-col h-screen">
       <Header />
       <div className="flex flex-col justify-center grow">
-        <BrowserRouter basename="/">
+        <HashRouter basename="/">
           <Routes>
             {/* Authentication Routes */}
             <Route path="/" element={<Login setUser={setUser} />} />
@@ -17,7 +23,7 @@ const App = () => {
             {/* Main Application Page */}
             <Route path="/app" element={<MainPage user={user} />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </div>
   );
